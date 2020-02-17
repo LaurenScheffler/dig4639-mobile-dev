7d46fd1387f96ccfdd8b44c43bad0b45e14c796b
Lauren Scheffler <la691274@ucf.edu>
2020-02-17 12:23:35 -0500

    ice 7

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,45 @@
-export function Sum (a, b) {
-  return a + b
+function Sum (a, b) {
+  let result = undefined
+ if(typeof a == "number" && typeof b == "number") {
+  result = a + b
+ }
+  return result;
 }
+
+ function AddList(Arr) {
+  let result = undefined;
+  if(Array.isArray(Arr) && Arr.length > 0){
+    result = 0;
+    for(var elem of Arr){
+      if(typeof elem != "number"){
+        result = undefined;
+        break;
+      }
+      result = result + elem;
+   }
+  }
+    return result;
+  }
+
+ function DivideBy(a, b) {
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number" == "zero") {
+    result = a / b;
+  }
+  return result;
+ }
+
+ function ContainsString(string) {
+   
+ }
+
+ function ReSortedNumbers(Array) {
+  if(typeof Array == "parameter"){
+    return ;
+  }
+ }
+
+ function Adder() {
+
+ }
+export { Sum, AddList };
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,10 +1,52 @@
-import { Sum } from './sum.js'
+import { Sum, AddList } from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+describe("AddList",() => {
+
+  test('Whether undefined is returned on invalid type', () => {
+    let numElem = 1000;
+    let input = [];
+    let result = 0;
+    var elem = 0;
+    for(var j = 0; j < 10; j++) {
+      for(var i = 0; i < numElem; i++){
+        elem = Math.random()*1000;
+        result += elem;
+        input.push(elem)
+      }
+      expect(AddList(input)).toBe(result);
+    }
+  })
+  test('Tests if empty array returns undefined', () => {
+    let input = [];
+    expect(AddList(input)).toBe(result);
+  })
+  test('Tests if undefined element returns undefined', () => {
+    let input = [undefined,0,2,3];
+    expect(AddList(input)).toBe(result);
+  })
+  test('Tests if non-array returns undefined', () => {
+
+  })
+})
+
+describe("Sum",() => {
+
+  test('Whether undefined is returned on invalid type', () => {
+    expect(Sum(1, "Test")).toBeUndefined()
+  })
+
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3)
+  })
+
+  it('produces the sum of 10 + 20, which should be 30', () => {
+    expect(Sum(10, 20)).toBe(30)
+  })
 })
 
 // ICE 
+
 
 // Examples for Null
 test('null', () => {
