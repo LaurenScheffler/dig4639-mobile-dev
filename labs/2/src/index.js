aae007c5900d2aee65c2f4d31888e32976a4224f
LaurenScheffler <Laureninmco@Knights.ucf.edu>
2020-02-21 19:03:45 -0500

    working on lab 3

--- labs/2/src/index.js
+++ labs/2/src/index.js
@@ -1,11 +1,11 @@
-import Card from "./components/Card/index.js" 
+import Card from './components/Card/index.js'
 
-function runOnLoad(){       
-    let element = document.createElement("div");    
-    element.id = "container";    
-    document.body.appendChild(element);    
-    var newCard = new Card({content:"Sample value provided"});    
-    element.appendChild(newCard.render());
+function runOnLoad () {
+  const element = document.createElement('div')
+  element.id = 'container'
+  document.body.appendChild(element)
+  var newCard = new Card({ content: 'Sample value provided' })
+  element.appendChild(newCard.render())
 }
 
-window.addEventListener("DOMContentLoaded", runOnLoad);
+window.addEventListener('DOMContentLoaded', runOnLoad)
