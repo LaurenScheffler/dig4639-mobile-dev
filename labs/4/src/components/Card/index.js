e17f56f7b409e551c9bcc309b64d3cae30886e1f
LaurenScheffler <Laureninmco@Knights.ucf.edu>
2020-02-28 17:41:26 -0500

    working on lab 4

---
+++ labs/4/src/components/Card/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import React from 'react';
+
+class Card extends React.Component {
+
+  render() { 
+    return  (<p className="card">This is a Card!</p>);
+  }
+  }
+
+export default Card; 
